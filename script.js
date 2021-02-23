6fcb1d3457b46409f76bd1a3b6c5b1506327ebf1
TalithadeKramer <76871148+TalithadeKramer@users.noreply.github.com>
2021-02-23 11:24:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/*var age = 17
+
+if (age > 18) {
+    document.write("Welkom!")
+}
+
+else {
+    document.write("Helaas bent u te jong om bij ons binnen te komen, we zien u graag een andere keer.")
+}*/
+
+/*var isFemale = "true"
+
+if (isFemale = true) {
+    document.write("Welkom!")
+}
+
+else {
+    document.write("Helaas is onze ladiesnight enkel voor dames toegankelijk, graag zien wij u een andere keer.")
+}*/
+
+var driverStatus = "bob"
+
+if (driverStatus = "bob") {
+    document.write("U kunt veilig naar huis rijden")
+}
+
+else {
+    document.write("Helaas bent u geen bob en kunt u beter niet meer rijden")
+}
